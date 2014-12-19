d9e67dce76135404dd673a2231b05a4d3932942b
Xavier Vela <x_vela@hotmail.com>
2014-12-19 12:17:11 -0500

    First try to publish
    Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #Variables privadas de la función makeCacheMatrix
+  
+  m<-NULL
+  
+  #Funciones privadas de la función makeCacheMatrix
+  
+  #Función set
+  set<-function(y) {
+    #El siguiente operador indica que se asigne el valor de y al x del ámbito padre; lo mismo m
+    x<<-y
+    m<<-NULL
+    
+  }
+  #Función get
+  get<-function() x
+  #Función setsolve
+  setsolve<-function(solve) m<<-solve
+  #Función getsolve
+  getsolve<-function() m
+  
+  #Retorno de la función cuando se llama a una variable de este tipo
+  list(set=set,get=get, setsolve=setsolve, getsolve=getsolve)
 }
 
 
@@ -12,4 +33,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
